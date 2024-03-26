86ace08bb3d54e9718becf612c2c27edbf6d576b
Daikone <eduardoarzolay@gmail.com>
2024-03-25 20:30:10 -0400

    refactor(neighborhoods): change Neighborhoods and ActionsBar styles, and change Neighborhoods markup

--- src/components/tab-agent/neighborhoods/Neighborhoods.jsx
+++ src/components/tab-agent/neighborhoods/Neighborhoods.jsx
@@ -9,28 +9,30 @@ const Neighborhoods = () => {
         <h2>Todos los Barrios</h2>
         <p>Estás viendo una lista con todos los barrios de la plataforma.</p>
       </header>
-      <ActionsBar />
-      <div className="table-responsive">
-        <table className="table align-middle" id="neighborhoods">
-          <thead>
-            <tr>
-              <th scope="col">Propiedades y detalles</th>
-              <th scope="col">Precio x Operación</th>
-              <th scope="col">Solicitudes</th>
-              <th scope="col">Ultima Acción</th>
-              <th scope="col">Favoritos y Compartido</th>
-              <th scope="col">Acción</th>
-            </tr>
-          </thead>
-          <tbody>
-            <Neighborhood />
-            <Neighborhood />
-            <Neighborhood />
-            <Neighborhood />
-          </tbody>
-        </table>
+      <div id="neighborhoods-container">
+        <ActionsBar />
+        <div className="table-responsive">
+          <table className="table align-middle" id="neighborhoods">
+            <thead>
+              <tr>
+                <th scope="col">Propiedades y detalles</th>
+                <th scope="col">Precio x Operación</th>
+                <th scope="col">Solicitudes</th>
+                <th scope="col">Ultima Acción</th>
+                <th scope="col">Favoritos y Compartido</th>
+                <th scope="col">Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Neighborhood />
+              <Neighborhood />
+              <Neighborhood />
+              <Neighborhood />
+            </tbody>
+          </table>
+        </div>
+        <Pagination />
       </div>
-      <Pagination />
     </>
   );
 };
